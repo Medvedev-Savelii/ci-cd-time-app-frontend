912b35f776a19351c46192d2aae133bc59364aaf
Sava <savamedvedev@gmail.com>
2023-07-19 15:04:54 +0800

    ci-3

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,38 +1,38 @@
-import moment from 'moment'
+import moment from 'moment';
 
 function startInterval() {
   setInterval(() => {
-    this.currentTime = moment().format('HH:mm:ss')
-  }, 1000)
+    this.currentTime = moment().format('HH:mm:ss');
+  }, 1000);
 }
 
 async function saveTime() {
-  const time = this.currentTime
-  const res = await fetch('http://localhost:5555/times', {
+  const time = this.currentTime;
+  const res = await fetch('http://158.160.48.1:5555/times', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ time }),
-  })
-  const json = await res.json()
+  });
+  const json = await res.json();
   if (json.insertId) {
-    this.savedTimes.unshift({ id: json.insertId, time })
-    this.$toast.success(`Время ${time} сохранено`, { position: 'top-right' })
+    this.savedTimes.unshift({ id: json.insertId, time });
+    this.$toast.success(`Время ${time} сохранено`, { position: 'top-right' });
   }
 }
 
 async function deleteTime(id) {
-  const res = await fetch(`http://localhost:5555/time/${id}`, {
+  const res = await fetch(`http://158.160.48.1:5555/time/${id}`, {
     method: 'DELETE',
-  })
-  const json = await res.json()
+  });
+  const json = await res.json();
   if (json.affectedRows) {
-    this.savedTimes = this.savedTimes.filter((savedTime) => savedTime.id !== id)
+    this.savedTimes = this.savedTimes.filter((savedTime) => savedTime.id !== id);
     this.$toast.error(`Время с ID ${id} удалено`, {
       position: 'top-right',
-    })
+    });
   }
 }
 
-export { startInterval, saveTime, deleteTime }
+export { startInterval, saveTime, deleteTime };
